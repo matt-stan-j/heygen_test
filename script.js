76923e2a74f570fbc428d8fd9b23d77465d29774
matt-stan-j <jusufstanley@gmail.com>
2025-07-17 11:23:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class HeyGenAWS {
         this.chatSessionId = this.generateUUID();
         
         // Replace with your API Gateway URL from the test
-        this.AWS_API_URL = 'https://YOUR-API-ID.execute-api.YOUR-REGION.amazonaws.com/prod';
+        this.AWS_API_URL = 'https://x4p585jeee.execute-api.ap-southeast-1.amazonaws.com/prod';
         
         this.initializeEventListeners();
         this.updateStatus('Ready to start');
